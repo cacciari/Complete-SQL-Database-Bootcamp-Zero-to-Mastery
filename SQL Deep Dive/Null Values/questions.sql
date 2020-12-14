d261acba72069c8fcf3a661de6ce67f5bdcfb102
cacciari <cacciari@gmail.com>
2020-12-14 15:23:36 -0300

    Update questions.sql

--- SQL Deep Dive/Null Values/questions.sql
+++ SQL Deep Dive/Null Values/questions.sql
@@ -5,8 +5,14 @@
 * Assuming a students minimum age for the class is 15, what is the average age of a student?
 */
 
+SELECT avg(coalesce(age, 15)) from "Student";
+
 /*
 * DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
 * Question: 
 * Replace all empty first or last names with a default?
 */
+SELECT coalesce(name, 'No name provided') as "Name",
+		coalesce(lastName, 'No name provided') as "Last Name",
+        coalesce(age, 15) as "Age"
+FROM "Student"
