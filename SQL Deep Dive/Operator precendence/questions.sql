50c188a859d416d39e5f354540330ee502b85c7c
cacciari <cacciari@gmail.com>
2020-12-14 11:22:18 -0300

    Update questions.sql

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -7,6 +7,10 @@
 * Include people that are 50
 * that are from either Japan or Australia
 */
+SELECT * FROM CUSTOMERS
+WHERE (AGE<30 OR AGE>=50)
+        AND income>50000 
+        AND (country='Japan' OR country='Australia');
 
 /*
 * DB: Store
@@ -14,3 +18,6 @@
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+SELECT SUM(totalamount) FROM ORDERS
+WHERE (orderdate >= '2004-07-01' AND orderdate <='2004-07-31')
+AND totalamount >100;
