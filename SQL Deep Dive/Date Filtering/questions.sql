2cb529069bbf73ab440645876d83f4748ebe553b
cacciari <cacciari@gmail.com>
2020-12-16 16:33:59 -0300

    Update questions.sql

--- SQL Deep Dive/Date Filtering/questions.sql
+++ SQL Deep Dive/Date Filtering/questions.sql
@@ -4,23 +4,30 @@
 * Question: Get me all the employees above 60, use the appropriate date functions
 */
 
-SELECT * FROM employees;
+select concat(first_name, ' ', last_name) AS "Name",
+    EXTRACT (YEAR FROM (age(now()::date, birth_date))) as YEAR, 'years'
+from employees
+WHERE birth_date < now() - interval '60 years'; 
 
 /*
 * DB: Employees
 * Table: employees
 * Question: How many employees where hired in February?
 */
-
-SELECT * FROM employees;
+select count(concat(first_name, ' ', last_name))
+from employees
+WHERE EXTRACT(MONTH FROM hire_date) = 2;
+--24448
 
 /*
 * DB: Employees
 * Table: employees
 * Question: How many employees were born in november?
 */
-
-SELECT * FROM employees;
+select count(concat(first_name, ' ', last_name))
+from employees
+WHERE EXTRACT(MONTH FROM birth_date) = 11;
+--24500
 
 /*
 * DB: Employees
@@ -36,5 +43,9 @@ SELECT * FROM employees;
 * Question: How many orders were made in January 2004?
 */
 
-SELECT * FROM orders;
+SELECt * FROM orders
+WHERE EXTRACT(MONTH FROM orderdate::Date) = 1
+        AND EXTRACT(YEAR FROM orderdate::Date) = 2004;
+                    
+                    
 
