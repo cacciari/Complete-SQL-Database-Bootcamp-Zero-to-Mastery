3bda2f1d8576cc47f2e8fca254865475ef08544e
cacciari <cacciari@gmail.com>
2020-12-17 14:25:25 -0300

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,6 +3,8 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
+select first_name, last_name FROm employees
+ORDER BY first_name ASC, last_name DESC;
 
 
 /*
@@ -10,6 +12,8 @@
 * Table: employees
 * Question: Sort employees by age
 */
+select first_name, last_name, EXTRACT ('YEAR' FROM age(birth_date)) FROm employees
+ORDER BY age(birth_date) DESC;
 
 
 /*
@@ -17,3 +21,8 @@
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+select first_name, last_name, hire_date FROm employees
+WHERE first_name LIKE 'K%'
+ORDER BY hire_date asc;
+                                                       
+                                                       
