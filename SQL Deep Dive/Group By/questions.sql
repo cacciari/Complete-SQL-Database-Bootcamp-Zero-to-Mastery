e00455442db66c89a060f15dc4c5c5cf574a358f
cacciari <cacciari@gmail.com>
2020-12-21 17:05:17 -0300

    Update questions.sql

--- SQL Deep Dive/Group By/questions.sql
+++ SQL Deep Dive/Group By/questions.sql
@@ -4,16 +4,23 @@
 *  Table: Employees
 */
 
-SELECT e.emp_no
+SELECT hire_date, count(hire_date) as "amount"
 FROM employees as e
+GROUP BY hire_date
+ORDER BY amount DESC
 
 /*
 *   Show me all the employees, hired after 1991 and count the amount of positions they've had
 *  Database: Employees
 */
 
-SELECT e.emp_no
-FROM employees as e
+select concat(e.first_name, ' ', e.last_name) as "Full Name",
+       e.hire_date AS "Hire Date", count(t.title) as "Positions"
+from employees as e 
+INNER JOIN titles as t USING (emp_no)
+WHERE EXTRACT (YEAR FROM e.hire_date) > 1991
+GROUP BY e.emp_no
+ORDER BY "Positions" DESC
 
 
 /*
@@ -21,5 +28,9 @@ FROM employees as e
 *  Database: Employees
 */
 
-SELECT e.emp_no
-FROM employees as e
+select de.dept_no, e.emp_no, concat(e.first_name, ' ', e.last_name) as "Name"
+FROm employees AS e
+INNER JOIN dept_emp as de USING(emp_no)
+INNER JOIN departments as dep USING (dept_no)
+WHERE dep.dept_name = 'Development'
+ORDER by e.emp_no
